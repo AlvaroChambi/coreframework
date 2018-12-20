c06bce6067c438a363838524a2c15636b0116baa
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2018-12-20 11:17:21 +0100

    Migrated profile classes

---
+++ src/main/java/es/developer/achambi/coreframework/ui/profile/BaseProfileFragment.java
@@ -0,0 +1,66 @@
+package es.developer.achambi.coreframework.ui.profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import es.developer.achambi.coreframework.R;
+import es.developer.achambi.coreframework.ui.Presenter;
+import es.developer.achambi.coreframework.ui.navigation.NavigationFragment;
+
+public abstract class BaseProfileFragment extends NavigationFragment implements View.OnClickListener {
+
+    private BaseProfilePresenter presenter;
+
+    @Override
+    public int getLayoutResource() {
+        return R.layout.profile_fragment_layout;
+    }
+
+    @Override
+    public Presenter setupPresenter() {
+        if( presenter == null ) {
+            presenter = new BaseProfilePresenter();
+        }
+        return presenter;
+    }
+
+    @Override
+    public void onViewSetup(View view, @Nullable Bundle savedInstanceState) {
+        View logoutButton = view.findViewById(R.id.profile_logout_button);
+        final TextView profileName = view.findViewById(R.id.profile_user_name_text);
+        ImageView profilePhoto = view.findViewById(R.id.profile_photo_image_view);
+        profileName.setText( presenter.getProfileName() );
+        Glide.with(getActivity())
+                .load(presenter.getPhotoUrl())
+                .apply(RequestOptions.circleCropTransform())
+                .into(profilePhoto);
+        logoutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public int getTitleResource() {
+        return R.string.profile_activity_title;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if( view.getId() == R.id.profile_logout_button ) {
+            FirebaseAuth.getInstance().signOut();
+            onUserLoggedOut();
+        }
+    }
+
+    public abstract void onUserLoggedOut();
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
