5b641198d509a9bbcac8af41f8f334b4bacf21f9
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2019-07-31 19:48:18 +0200

    Fixed bug closing softkeyboard

--- src/main/java/es/developer/achambi/coreframework/utils/WindowUtils.java
+++ src/main/java/es/developer/achambi/coreframework/utils/WindowUtils.java
@@ -3,11 +3,15 @@ package es.developer.achambi.coreframework.utils;
 import android.app.Activity;
 import android.view.inputmethod.InputMethodManager;
 
+import java.util.Objects;
+
 public class WindowUtils {
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager)
                 activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(
-                activity.getCurrentFocus().getWindowToken(), 0);
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(
+                    Objects.requireNonNull(activity.getCurrentFocus()).getWindowToken(), 0);
+        }
     }
 }
