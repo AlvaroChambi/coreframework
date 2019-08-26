0ed16ad427136e502740d2d5eb94f81bd6660df0
achambi <alvaro.chambi.campos@gmail.com>
2019-08-26 18:19:51 +0200

    Added paginated model

--- src/main/java/es/developer/achambi/coreframework/ui/pagination/PaginatedData.kt
+++ src/main/java/es/developer/achambi/coreframework/ui/pagination/PaginatedData.kt
@@ -2,4 +2,5 @@ package es.developer.achambi.coreframework.ui.pagination
 
 abstract class PaginatedData<D>( val data: ArrayList<D> = ArrayList(),
                         var endPage: Boolean = false,
+                        var count: Int = 0,
                         var nextPageIndex: Int = 0 )
