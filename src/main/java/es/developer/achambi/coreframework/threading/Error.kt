70832daa6da0892af246dc2ee626f24c0c633208
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2020-01-06 12:56:54 +0100

    Added error types

--- src/main/java/es/developer/achambi/coreframework/threading/Error.kt
+++ src/main/java/es/developer/achambi/coreframework/threading/Error.kt
@@ -1,3 +1,4 @@
 package es.developer.achambi.coreframework.threading
 
-class Error(message: String = "") : Exception(message)
+class Error(message: String? = "",
+            val type: String? = null) : Exception(message)
