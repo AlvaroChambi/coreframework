a502311fd9ecf21d4ff940041ecf310a43cd574d
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2020-01-24 11:41:05 +0100

    Updated date format util

--- src/main/java/es/developer/achambi/coreframework/utils/DateFormatUtils.kt
+++ src/main/java/es/developer/achambi/coreframework/utils/DateFormatUtils.kt
@@ -4,9 +4,15 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object DateFormatUtils {
-    private val YEAR_MONTH_DAY_HOUR_MINUTE_SECONDS = "yyyyMMdd_HHmmss"
+    private const val YEAR_MONTH_DAY_HOUR_MINUTE_SECONDS = "yyyyMMdd_HHmmss"
+    private const val DAY_MONTH_YEAR = "dd MMM yyyy"
     fun formatDateDetailed(date: Date) : String {
         val dateFormat = SimpleDateFormat(YEAR_MONTH_DAY_HOUR_MINUTE_SECONDS, Locale.getDefault())
+        return dateFormat.format(date)
+    }
+
+    fun formatDateSimplified(date: Date): String {
+        val dateFormat = SimpleDateFormat(DAY_MONTH_YEAR, Locale.getDefault())
         return dateFormat.format(date)
     }
 }
